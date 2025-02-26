eab3f8628cfd6258b7694435d8e6a7ae415f9ed0
EmilTroejborg <next16163@edu.nextkbh.dk>
2025-02-26 09:45:55 +0100

    playerMovement v1

--- emilt.js
+++ emilt.js
@@ -1,7 +1,24 @@
-function emilSetup(){
+let playerX = 200;
+let playerY = 200;
+let playerD = 60;
+let playerXSpeed = 10;
+let playerYSpeed = 10;
+
+function setup(){
+    createCanvas(windowWidth, windowHeight);
+    
+}
+function player(){
+
 
 }
+function Movement(button, playerXOrY, movementDirection){
+    if(keyIsDown(button) && movementDirection == left){
+        playerXOrY 
+    }
+}
 
-function emilDraw(){
-    circle(300,50,20)
+function draw(){
+    background(200);
+    circle(playerX, playerY, playerD);
 }
