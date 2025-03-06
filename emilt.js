b4db0db98f494db60db0f0ef93bad093e0ae7136
EmilTroejborg <next16163@edu.nextkbh.dk>
2025-03-06 14:44:30 +0100

    Cat

--- emilt.js
+++ emilt.js
@@ -5,20 +5,28 @@ let playerXSpeed = 10;
 let playerYSpeed = 10;
 
 function setup(){
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(1000, 600);
     
 }
 function player(){
 
 
 }
-function Movement(button, playerXOrY, movementDirection){
-    if(keyIsDown(button) && movementDirection == left){
-        playerXOrY 
-    }
-}
+
 
 function draw(){
     background(200);
+    if(keyIsDown(65)){ //left
+        playerX -= playerXSpeed
+    }
+    if(keyIsDown(68)){ //right
+        playerX += playerXSpeed
+    }
+    if(keyIsDown(87)){ //up
+        playerY -= playerYSpeed
+    }
+    if(keyIsDown(83)){ //down
+        playerY += playerYSpeed
+    }
     circle(playerX, playerY, playerD);
 }
