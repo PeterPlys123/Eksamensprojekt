5c0d6b6721490aa9a4e706b969d6df8b2a42d7da
EmilTroejborg <next16163@edu.nextkbh.dk>
2025-03-12 09:18:08 +0100

    tyngdekraft og centrering af canvas

--- emilt.js
+++ emilt.js
@@ -1,21 +1,35 @@
-let playerX = 200;
-let playerY = 200;
+let cnvwidth = 1500;
+let cnvheight = 600;
+let cnv; //canvas
+
 let playerD = 60;
-let playerXSpeed = 10;
-let playerYSpeed = 10;
+let playerX = cnvwidth / 2;
+let playerY = cnvheight - playerD / 2;
+
+let playerYVelocity;
+let playerXSpeed = 7;
+let playerYSpeed = 7;
+
+
 
 function setup(){
-    createCanvas(1000, 600);
-
+    cnv = createCanvas(cnvwidth, cnvheight);
+    cnv.position(windowWidth - (windowWidth + cnvwidth) / 2, 0)
 }
-function player(){
+function playerGravity(){
 
+    let playerGravity = 0.5;
+    let playerGround = cnvheight;
 
+    if(playerY + playerD / 2 < playerGround){
+        playerYVelocity += playerGravity;
+        playerY += playerYVelocity;
+    } else{
+        playerYVelocity = 0;
+    }
 }
 
-
-function draw(){
-    background(200);
+function playerMovement(){
     if(keyIsDown(65)){ //left
         playerX -= playerXSpeed
     }
@@ -25,8 +39,18 @@ function draw(){
     if(keyIsDown(87)){ //up
         playerY -= playerYSpeed
     }
-    if(keyIsDown(83)){ //down
-        playerY += playerYSpeed
-    }
     circle(playerX, playerY, playerD);
+}
+  
+function verticalScroll(){
+    let scroll;
+
+    translate (0, scroll)
+}
+
+
+function draw(){
+    background(200);
+    playerMovement();
+    //playerGravity();
 }
