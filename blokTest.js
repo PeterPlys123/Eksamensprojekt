2e03307aed7560157f5a7fefce27b4fae568e44a
PeterPlys <mads@dmrn.dk>
2025-03-12 09:45:12 +0100

    12 - 3 - Slut på dagen

--- blokTest.js
+++ blokTest.js
@@ -1,17 +1,15 @@
 let blokArray
+let farveArray
 
 blokArray = [20, 120, 220, 320, 420]
+farveArray = ['gray', 'blue', 'yellow', 'black', 'brown']
 
 function blokDraw(){
-fill('gray')
-rect(blokArray[0], 360, blokArray[0]+75, 380)
-fill('blue')
-rect(blokArray[1], 360, blokArray[1]+75, 380)
-fill('yellow')
-rect(blokArray[2], 360, blokArray[2]+75, 380)
-fill('black')
-rect(blokArray[3], 360, blokArray[3]+75, 380)
-fill('brown')
-rect(blokArray[4], 360, blokArray[4]+75, 380)
+
+for(let i = 0; i == 4; i++)
+{
+fill(farveArray[i])
+rect(blokArray[i], 360, blokArray[i]+75, 380)
+}
 
 }
