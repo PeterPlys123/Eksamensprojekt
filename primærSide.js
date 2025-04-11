29a79ef339560012d8451b79935dad0d233eea0e
PeterPlys <mads@dmrn.dk>
2025-04-11 15:17:14 +0200

    Slutningen på dagen - 11 - 4

--- primærSide.js
+++ primærSide.js
@@ -1,23 +1,44 @@
-let canvas, baggrund, ground, playerVelY
-let bBlokArrayX, bBlokArrayY, farveArray, blokBredde, blokTyk, isBlokArrayY, isBlokArrayX, pPlayerX, newPlayerX, hastighed, playerVelX
-ground = false
-playerVelY = 0
-let collision
-blokBredde = 75
-blokTyk = 20
-function setup() {
-	createCanvas(800, 400);
-    bBlokArrayX = [20, 140, 260, 380, 500]
-    bBlokArrayY = [100, 20, 300, 270, 185]
-    isBlokArrayX = [20, 140, 260, 380, 500]
-    isBlokArrayY = [100, 20, 300, 270, 185]
+let pPlayerX,
+newPlayerX,
+hastighed,
+playerVelX
 
-    farveArray = ['black', 'red', 'gray', 'yellow', 'lightblue']
+let ground = false
+let playerVelY = 0
+let collision
+let blokBredde = 75
+let blokTyk = 20
+
+let trampHop = -15
+let blokKolStop = 1
+
+let bBlokArrayX = [150]
+let bBlokArrayY = [50]
+
+let speedBlokArrayX = [250]
+let speedBlokArrayY = [125]
+
+let sandBlokArrayX = [350]
+let sandBlokArrayY = [250]
+
+let trampBlokArrayX = [450]
+let trampBlokArrayY = [375]
+
+let fældeBlokArrayX = [550]
+let fældeBlokArrayY = [400]
+
+let farveArray = ['black', 'red', 'gray', 'yellow', 'lightblue']
+
+
+function setup()
+{
+	createCanvas(800, 400);
+
 }
 
 function draw()
 {
-    frameRate(45)
+    frameRate(60)
     background('lightgreen')
     testBevægelse()
 
@@ -29,45 +50,124 @@ function draw()
     //Tyngdekraft
     //Fungere ved at der er en ground, som skifter mellem true og false.
     //Når der er kollision med en platform er den true, og når man hopper, eller går over kanten er den false.
-if(ground == false){
+if(ground == false)
+{
 playerVelY += 0.5
 playerY += playerVelY
 }
 
-
+//Her tegnes basis blokkene
 for(let i = 0; i < bBlokArrayX.length; i++)
     {
     fill(farveArray[2])
     rect(bBlokArrayX[i], bBlokArrayY [i], blokBredde, blokTyk)
     }
 
-    
-    for(let i = 0; i < bBlokArrayY.length; i++){
-        if(playerY + playerR > bBlokArrayY[i] && playerY - playerR < bBlokArrayY[i]){
-            if(playerX + playerR > bBlokArrayX[i] && playerX - playerR <bBlokArrayX[i] + blokBredde){
+//Her kigges der på kollision.
+    for(let i = 0; i < bBlokArrayY.length; i++)
+    {
+        if(playerY + playerR > bBlokArrayY[i] && playerY - playerR < bBlokArrayY[i])
+        {
+            if(playerX + playerR > bBlokArrayX[i] && playerX - playerR <bBlokArrayX[i] + blokBredde)
+            {
                 bBlok(i)
             }
-            else{
+            else
+            {
                 ground = false
             }
         }
     }
 
-    for(let i = 0; i < isBlokArrayX.length; i++)
+//Her tegnes speed blokkene
+    for(let i = 0; i < speedBlokArrayX.length; i++)
         {
         fill(farveArray[4])
-        rect(isBlokArrayX[i], isBlokArrayY [i], blokBredde, blokTyk)
+        rect(speedBlokArrayX[i], speedBlokArrayY [i], blokBredde, blokTyk)
         }
     
-        
-        for(let i = 0; i < isBlokArrayY.length; i++){
-            if(playerY + playerR > isBlokArrayY[i] && playerY - playerR < isBlokArrayY[i]){
-                if(playerX + playerR > isBlokArrayX[i] && playerX - playerR <isBlokArrayX[i] + blokBredde){
+//Her kigges der på kollision.
+        for(let i = 0; i < speedBlokArrayY.length; i++)
+        {
+            if(playerY + playerR > speedBlokArrayY[i] && playerY - playerR < speedBlokArrayY[i])
+            {
+                if(playerX + playerR > speedBlokArrayX[i] && playerX - playerR < speedBlokArrayX[i] + blokBredde)
+                {
                     isBlok(i)
                 }
-                else{
+                else
+                {
                     ground = false
                 }
             }
         }
+
+//Her tegnes sandBlokken.
+for(let i = 0; i < sandBlokArrayX.length; i++)
+    {
+    fill(farveArray[3])
+    rect(sandBlokArrayX[i], sandBlokArrayY [i], blokBredde, blokTyk)
     }
+
+//Her kigges der på kollision.
+    for(let i = 0; i < sandBlokArrayY.length; i++)
+    {
+        if(playerY + playerR > sandBlokArrayY[i] && playerY - playerR < sandBlokArrayY[i])
+        {
+            if(playerX + playerR > sandBlokArrayX[i] && playerX - playerR < sandBlokArrayX[i] + blokBredde)
+            {
+                sandBlok(i)
+            }
+            else
+            {
+                ground = false
+            }
+        }
+    }
+
+//Her tegnes trampolinblokkene.
+    for(let i = 0; i < trampBlokArrayX.length; i++)
+        {
+        fill(farveArray[0])
+        rect(trampBlokArrayX[i], trampBlokArrayY [i], blokBredde, blokTyk)
+        }
+    
+    //Her kigges der på kollision.
+        for(let i = 0; i < trampBlokArrayY.length; i++)
+        {
+            if(playerY + playerR > trampBlokArrayY[i] && playerY - playerR < trampBlokArrayY[i])
+            {
+                if(playerX + playerR > trampBlokArrayX[i] && playerX - playerR < trampBlokArrayX[i] + blokBredde)
+                {
+                    trampBlok(i)
+                }
+                else
+                {
+                    ground = false
+                }
+            }
+        }
+
+//Her tegnes fælde blokken.
+        for(let i = 0; i < fældeBlokArrayX.length; i++)
+            {
+            fill(farveArray[0])
+            rect(fældeBlokArrayX[i], fældeBlokArrayY [i], blokBredde, blokTyk)
+            }
+        
+        //Her kigges der på kollision.
+            for(let i = 0; i < fældeBlokArrayY.length; i++)
+            {
+                if(playerY + playerR > fældeBlokArrayY[i] && playerY - playerR < fældeBlokArrayY[i])
+                {
+                    if(playerX + playerR > fældeBlokArrayX[i] && playerX - playerR < fældeBlokArrayX[i] + blokBredde)
+                    {
+                        fældeBlok(i)
+                    }
+                    else
+                    {
+                        ground = false
+                    }
+                }
+            }
+}
